d0552c6c7b2748c3de61f4faae6fa4dc67e91118
gilgamesh7 <felidaesrule@yahoo.com>
2023-01-10 10:52:34 +1300

    Tokenise a file

--- basics.py
+++ basics.py
@@ -1,6 +1,9 @@
 # Test if correct package installed & language loaded
 
 import spacy
+from pathlib import Path 
+
+
 nlp = spacy.load("en_core_web_sm") 
 print(f"Language callable object : {nlp}")
 
@@ -13,6 +16,14 @@ introduction_doc = nlp(
     "This tutorial is about Natural Language Processing in spaCy."
 )
 print(f"Type of doc : {type(introduction_doc)}")
+
+print(f"Tokens : {[token.text for token in introduction_doc]}")
+
+# Read from a file
+file_name = Path.cwd()/"documents"/"example.txt"
+introduction_doc = nlp(
+    file_name.read_text(encoding="utf-8")
+)
 
 print(f"Tokens : {[token.text for token in introduction_doc]}")
 
