331c3811251ba4894c77fcdd95907fba2552f6e4
gilgamesh7 <felidaesrule@yahoo.com>
2023-01-13 18:18:41 +1300

    get index of token

--- basics.py
+++ basics.py
@@ -53,3 +53,15 @@ custom_ellipsis_doc = custom_nlp(ellipsis_text)
 custom_ellipsis_sentences = list(custom_ellipsis_doc.sents)
 for sentence in custom_ellipsis_sentences:
     print(sentence)
+
+# Get token index
+about_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech"
+    " company. He is interested in learning"
+    " Natural Language Processing."
+)
+about_doc = nlp(about_text)
+
+for token in about_doc:
+    print (token, token.idx)
