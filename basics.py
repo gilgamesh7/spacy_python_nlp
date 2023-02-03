d4decb3f3ad843501cc79e5036210499538f3467
gilgamesh7 <felidaesrule@yahoo.com>
2023-02-03 08:40:39 +0530

    word frequency

--- basics.py
+++ basics.py
@@ -139,3 +139,45 @@ print("Print tokens tht have been lemmatized : ")
 for token in conference_help_doc:
     if str(token) != str(token.lemma_):
         print(f"{str(token):>20} : {str(token.lemma_)}")
+
+# Word Frequency
+import spacy
+from collections import Counter
+
+nlp = spacy.load("en_core_web_sm")
+
+complete_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech company. He is"
+    " interested in learning Natural Language Processing."
+    " There is a developer conference happening on 21 July"
+    ' 2019 in London. It is titled "Applications of Natural'
+    ' Language Processing". There is a helpline number'
+    " available at +44-1234567891. Gus is helping organize it."
+    " He keeps organizing local Python meetups and several"
+    " internal talks at his workplace. Gus is also presenting"
+    ' a talk. The talk will introduce the reader about "Use'
+    ' cases of Natural Language Processing in Fintech".'
+    " Apart from his work, he is very passionate about music."
+    " Gus is learning to play the Piano. He has enrolled"
+    " himself in the weekend batch of Great Piano Academy."
+    " Great Piano Academy is situated in Mayfair or the City"
+    " of London and has world-class piano instructors."
+)
+
+complete_doc = nlp(complete_text)
+
+words = [
+    token.text
+    for token in complete_doc
+    if not token.is_stop and not token.is_punct
+]
+
+print(f"Removing stop words & punctuations : \n {Counter(words).most_common(5)}")
+
+words = [
+    token.text
+    for token in complete_doc
+]
+
+print(f"WITHOUT     removing stop words & punctuations : \n {Counter(words).most_common(5)}")
